2606201634f3eed6664a61b35d845ab185851409
Quan Tong <quantonganh@gmail.com>
2022-03-23 13:33:51 +0700

    add custom handler to distinguish between client and server errors

--- error.go
+++ error.go
@@ -1,1 +1,39 @@
 package todo
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+)
+
+const (
+	ErrInvalid  = "invalid"
+	ErrNotFound = "not_found"
+	ErrConflict = "conflict"
+)
+
+var (
+	ErrTaskNotFound = errors.New("task not found")
+)
+
+// Error represents an app error
+// This can be used to distinguish between client and server error
+type Error struct {
+	Code    string `json:"code"`
+	Message string `json:"message,omitempty"`
+	Err     error  `json:"err,omitempty"`
+}
+
+func (e *Error) Error() string {
+	var buf bytes.Buffer
+
+	if e.Err != nil {
+		buf.WriteString(e.Err.Error())
+	} else {
+		if e.Code != "" {
+			fmt.Fprintf(&buf, "<%s>", e.Code)
+		}
+		buf.WriteString(e.Message)
+	}
+	return buf.String()
+}
--- http/error.go
+++ http/error.go
@@ -1,1 +1,47 @@
 package http
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+
+	"github.com/quantonganh/todo"
+)
+
+var errorToStatusCode = map[string]int{
+	todo.ErrInvalid:  http.StatusBadRequest,
+	todo.ErrConflict: http.StatusBadRequest,
+	todo.ErrNotFound: http.StatusNotFound,
+}
+
+type appHandler func(c *gin.Context) error
+
+func (s *Server) Error(fn appHandler) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		err := fn(c)
+		if err == nil {
+			return
+		}
+
+		var ve validator.ValidationErrors
+		if errors.As(err, &ve) {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		clientError := new(todo.Error)
+		if errors.As(err, &clientError) {
+			c.JSON(errorToStatusCode[clientError.Code], clientError)
+			return
+		}
+
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "Oops! Something went wrong.",
+		})
+		s.logger.Err(err).Msg("")
+	}
+}
