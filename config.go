d1e3a6a8ffcda81f69ad03b58be9c3e261a14265
Quan Tong <quantonganh@gmail.com>
2022-03-23 13:34:36 +0700

    add config

--- config.go
+++ config.go
@@ -1,1 +1,12 @@
 package todo
+
+// Config represents the app config
+type Config struct {
+	AppPort string `envconfig:"APP_PORT"`
+
+	DBHost     string `envconfig:"DB_HOST"`
+	DBPort     string `envconfig:"DB_PORT"`
+	DBUser     string `envconfig:"DB_USER"`
+	DBPassword string `envconfig:"DB_PASSWORD"`
+	DBName     string `envconfig:"DB_NAME"`
+}
